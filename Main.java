71a006f71bd51a529148d145afb3b9ba38331883
Tatyana <fedoseevatatyana9@gmail.com>
2020-11-24 12:26:09 +0300

    new commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int transaction = 500000000;
+        int balance_new = balance + transaction;
+        System.out.println(balance_new);
+    }
+}
